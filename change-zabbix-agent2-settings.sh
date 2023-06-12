b4ec4a187c191a443734bceb966dea70570bf434
MK <mihael.kartashev@gmail.com>
2023-06-12 19:44:45 +0300

    add s/^ServerActive=/#ServerActive=/

--- change-zabbix-agent2-settings.sh
+++ change-zabbix-agent2-settings.sh
@@ -13,6 +13,7 @@ if [ -z "$1" ]; then
     echo "cp /etc/zabbix/zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf.bak"
     echo "sed -i -r 's/^Hostname=.*/Hostname=%s/' /etc/zabbix/zabbix_agent2.conf" "$1"
     echo "sed -i -r 's/^Server=.*/Server=%s/' /etc/zabbix/zabbix_agent2.conf" "$2"
+    echo "sed -i -r 's/^ServerActive=/#ServerActive=/' /etc/zabbix/zabbix_agent2.conf" "$2"
 else
     echo
     echo "cp /etc/zabbix/zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf.bak"
@@ -21,12 +22,17 @@ else
     grep Hostname= /etc/zabbix/zabbix_agent2.conf
     echo "grep Server= /etc/zabbix/zabbix_agent2.conf"
     grep Server= /etc/zabbix/zabbix_agent2.conf
+    echo "grep ServerActive= /etc/zabbix/zabbix_agent2.conf"
+    grep ServerActive= /etc/zabbix/zabbix_agent2.conf
     echo
     printf "### try to update /etc/zabbix/zabbix_agent2.conf with command: sed -i -r s/^Hostname=.*/Hostname=%s/ /etc/zabbix/zabbix_agent2.conf" "$1"
     sed -i -r "s/^Hostname=.*/Hostname=$1/" /etc/zabbix/zabbix_agent2.conf
     echo
     printf "### try to update /etc/zabbix/zabbix_agent2.conf with command: sed -i -r s/^Server=.*/Server=%s/ /etc/zabbix/zabbix_agent2.conf" "$2"
     sed -i -r "s/^Server=.*/Server=$2/" /etc/zabbix/zabbix_agent2.conf
+    echo
+    printf "### try to update /etc/zabbix/zabbix_agent2.conf with command: sed -i -r s/^Server=.*/Server=%s/ /etc/zabbix/zabbix_agent2.conf" "$2"
+    sed -i -r "s/^ServerActive=/#ServerActive=/" /etc/zabbix/zabbix_agent2.conf
     echo
     echo "### Checking result ###"
     echo
