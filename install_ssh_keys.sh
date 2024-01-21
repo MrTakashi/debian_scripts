52f6cbe0465242d6a60bd86ea32df9b75de373cf
MK <mihael.kartashev@gmail.com>
2024-01-21 14:03:27 +0300

    ~/.ssh/config instead of /root/ssh_keys/ssh_config.d/no_strict_host_key_checking.conf

--- install_ssh_keys.sh
+++ install_ssh_keys.sh
@@ -5,22 +5,23 @@
 
 if [ ! -d "/root/ssh_keys" ]
 then
-    echo "You have to add keys folder 'ssh_keys' to /root, before running the script"
+    echo "You have to add folder 'ssh_keys' with keys to /root, before running the script. Exit."
     exit 1
 fi
 
 echo
-echo "[ Script will do several tasks ]"
-echo "[ 1 ] install root's ssh-keys"
-echo "[ 2 ] install mk's ssh-keys"
-echo "[ 3 ] install mk's ssh key for github.com and setup ssh-connection (github.com.conf)"
-echo "[ 4 ] disable PasswordAuthentication for ssh (keys auth only)"
-echo "[ 5 ] configuring no strict host key checking"
-echo "[ 6 ] restart sshd service"
-echo "[ 7 ] testing github connection"
-echo "[ 8 ] remove ssh_keys folder"
+echo "[ Script will do several tasks: ]"
+echo "[ 1 ] install root's keys"
+echo "[ 2 ] install mk's keys"
+echo "[ 3 ] Create ~/.ssh/config for root"
+echo "[ 4 ] Create ~/.ssh/config for mk"
+echo "[ 5 ] Disable PasswordAuthentication"
+echo "[ 6 ] Restart sshd"
+echo "[ 7 ] Show test commands"
+echo "[ 8 ] Remove ssh_keys folder"
 echo
-echo "[1] installing keys for root"
+
+echo "[1] Install root's keys"
 echo "Preparing folder for public keys: mkdir ~/.ssh -p && chmod 700 ~/.ssh"
 mkdir ~/.ssh -p && chmod 700 ~/.ssh && echo "[OK]"
 echo "Coping public key: cp /root/ssh_keys/root/id_ed25519.pub ~/.ssh/authorized_keys"
@@ -33,7 +34,8 @@ cp /root/ssh_keys/root/id_ed25519 ~/.ssh/id_ed25519 && echo "[OK]"
 echo "Changing access: chmod 400 ~/.ssh/id_ed25519"
 chmod 400 ~/.ssh/id_ed25519 && echo "[OK]"
 echo
-echo "[2] installing keys for mk"
+
+echo "[2] Install mk's keys"
 echo "Preparing folder for public keys: mkdir /home/mk/.ssh -p && chmod 700 /home/mk/.ssh && chown mk:mk /home/mk/.ssh"
 mkdir /home/mk/.ssh -p && chmod 700 /home/mk/.ssh && chown mk:mk /home/mk/.ssh && echo "[OK]"
 echo "Coping public key: cp /root/ssh_keys/mk/id_ed25519.pub /home/mk/.ssh/authorized_keys"
@@ -41,50 +43,46 @@ cp /root/ssh_keys/mk/id_ed25519.pub /home/mk/.ssh/authorized_keys && echo "[OK]"
 echo "Changing access: chmod 600 /home/mk/.ssh/authorized_keys && chown mk:mk /home/mk/.ssh/authorized_keys"
 chmod 600 /home/mk/.ssh/authorized_keys && chown mk:mk /home/mk/.ssh/authorized_keys && echo "[OK]"
 echo
-
 echo "Coping private key: cp /root/ssh_keys/mk/id_ed25519 /home/mk/.ssh/id_ed25519"
 cp /root/ssh_keys/mk/id_ed25519 /home/mk/.ssh/id_ed25519 && echo "[OK]"
 echo "Changing access: chmod 400 /home/mk/.ssh/id_ed25519 && chown mk:mk /home/mk/.ssh/id_ed25519"
 chmod 400 /home/mk/.ssh/id_ed25519 && chown mk:mk /home/mk/.ssh/id_ed25519 && echo "[OK]"
 echo
-
-echo "[3] configuring ssh-connection to github.com for mk"
-echo "Preparing folder for ssh-config: mkdir /etc/ssh/ssh_config.d"
-mkdir /etc/ssh/ssh_config.d && echo "[OK]"
-
-echo "Coping ssh-config: cp /root/ssh_keys/ssh_config.d/github.com.conf /etc/ssh/ssh_config.d/github.com.conf"
-cp /root/ssh_keys/ssh_config.d/github.com.conf /etc/ssh/ssh_config.d/github.com.conf && echo "[OK]"
-
-echo "Coping private key: cp /root/ssh_keys/mk/mk_github_ed25519 /home/mk/.ssh/mk_github_ed25519"
+echo "Coping private key for github.com: cp /root/ssh_keys/mk/mk_github_ed25519 /home/mk/.ssh/mk_github_ed25519"
 cp /root/ssh_keys/mk/mk_github_ed25519 /home/mk/.ssh/mk_github_ed25519 && echo "[OK]"
-echo "Changing access: chmod 400 /home/mk/.ssh/mk_github_ed25519 && chown mk:mk /home/mk/.ssh/mk_github_ed25519"
-chmod 400 /home/mk/.ssh/mk_github_ed25519 && chown mk:mk /home/mk/.ssh/mk_github_ed25519 && echo "[OK]"
+echo "Changing access: chmod 400 /home/mk/.ssh/id_ed25519 && chown mk:mk /home/mk/.ssh/id_ed25519"
+chmod 400 /home/mk/.ssh/id_ed25519 && chown mk:mk /home/mk/.ssh/id_ed25519 && echo "[OK]"
 echo
 
-echo "[4] disabling PasswordAuthentication"
-echo "Edit /etc/ssh/sshd_config: sed -i -r 's/^#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config"
-sed -i -r 's/^#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config && echo "[OK]"
-grep -e "^PasswordAuthentication" /etc/ssh/sshd_config
+echo "[3] Create ~/.ssh/config for root (disable strict host key checking)"
+echo "cp /root/ssh_keys/root/config /root/.ssh/config"
+cp /root/ssh_keys/root/config /root/.ssh/config && echo "[OK]"
+echo "chmod 600 /root/.ssh/config"
+chmod 600 /root/.ssh/config && echo "[OK]"
+
+echo "[4] Create ~/.ssh/config for mk (disable strict host key checking + specify key for github.com)"
+echo "cp /root/ssh_keys/mk/config /home/mk/.ssh/config"
+cp /root/ssh_keys/mk/config /home/mk/.ssh/config && echo "[OK]"
+echo "chown mk:mk /home/mk/.ssh/config && chmod 600 /home/mk/.ssh/config"
+chown mk:mk /home/mk/.ssh/config && chmod 600 /home/mk/.ssh/config && echo "[OK]"
+
+echo "[5] Disabling PasswordAuthentication: \"PasswordAuthentication no\" > /etc/ssh/sshd_config.d/disable_password_auth.conf"
+echo "PasswordAuthentication no" > /etc/ssh/sshd_config.d/disable_password_auth.conf
 echo
 
-echo "[5] configuring no strict host key checking"
-echo "Coping ssh-config: cp /root/ssh_keys/ssh_config.d/github.com.conf /etc/ssh/ssh_config.d/github.com.conf"
-cp /root/ssh_keys/ssh_config.d/no_strict_host_key_checking.conf /etc/ssh/ssh_config.d/no_strict_host_key_checking.conf && echo "[OK]"
-echo
-
-echo "[6] restarting sshd-server"
+echo "[6] Restarting sshd"
 systemctl restart sshd && echo [OK]
 echo "sshd server restarted and you can try to connect with ssh-keys"
 echo
 
-echo "[7] for testing purpose you can use commands:"
-echo "runuser -l mk -c 'ssh -T git@github.com'"
+echo "[7] Test commands"
+echo "su mk -c 'ssh -T git@github.com'"
 echo "runuser -l mk -c 'ssh -vT git@github.com'"
 echo "ssh 10.10.10.200"
 echo "ssh 10.10.20.200"
 echo
 
-echo "[8] removing ssh_keys folder"
+echo "[8] Removing ssh_keys folder"
 echo "if all test passed you can remove key folder with command:"
 echo "rm -r /root/ssh_keys"
 echo
