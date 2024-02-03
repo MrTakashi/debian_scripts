82fec58a12d284b2202fa6ffced3eece97632398
MK <mihael.kartashev@gmail.com>
2024-02-03 23:09:17 +0300

    add new lines

--- install_user_settings.sh
+++ install_user_settings.sh
@@ -1,17 +1,24 @@
 echo "[ Script will do several tasks: ]"
 echo "[ 1 ] install ~/.bashrc for root and for mk"
 echo "[ 2 ] install ~/.vimrc for root and for mk"
+echo
 
 echo "Install ~/.bashrc for root:"
 echo "wget -qO - http://i.sspx.ru/1 >> ~/.bashrc"
 wget -qO - http://i.sspx.ru/1 >> ~/.bashrc && echo "[OK]"
+echo
+
 echo "Install ~/.bashrc for mk:"
 echo "runuser -l mk -c 'wget -qO - http://i.sspx.ru/1 >> ~/.bashrc'"
 runuser -l mk -c 'wget -qO - http://i.sspx.ru/1 >> ~/.bashrc' && echo "[OK]"
+echo
 
 echo "Install ~/.vimrc for root:"
 echo "wget -qO - http://i.sspx.ru/2 >> ~/.vimrc"
 wget -qO - http://i.sspx.ru/2 >> ~/.bashrc && echo "[OK]"
+echo
+
 echo "Install ~/.vimrc for mk:"
 echo "runuser -l mk -c 'wget -qO - http://i.sspx.ru/2 >> ~/.vimrc'"
 runuser -l mk -c 'wget -qO - http://i.sspx.ru/2 >> ~/.vimrc' && echo "[OK]"
+echo
