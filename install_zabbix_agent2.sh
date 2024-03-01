908429fec7c0c466556da4d41dcd23999703a6e4
MK <mihael.kartashev@gmail.com>
2024-03-01 13:21:02 +0300

    change default server to pru.sspx.ru; add menu to select version;

--- install_zabbix_agent2.sh
+++ install_zabbix_agent2.sh
@@ -6,7 +6,7 @@
 # wget -qO - https://raw.githubusercontent.com/MrTakashi/debian_scripts/main/install_zabbix_agent2.sh | bash
 
 # Default Zabbix Server IP
-default_zabbix_server_ip="10.10.20.120"
+default_zabbix_server_address="pru.sspx.ru"
 default_agent_hostname=$(uname -n)
 
 # Function to check if the script is running with root privileges
@@ -17,11 +17,52 @@ check_root_privileges() {
     fi
 }
 
+# Function to choose Zabbix Agent 2 version to install
+display_menu() {
+    echo "Select Zabbix version to install:"
+    options=("6.0" "6.4" "Quit")
+    select opt in "${options[@]}"; do
+        case "$opt" in
+            "6.0")
+                install_zabbix_agent "6.0"
+                break
+                ;;
+            "6.4")
+                install_zabbix_agent "6.4"
+                break
+                ;;
+            "Quit")
+                echo "Exiting..."
+                break
+                ;;
+            *)
+                echo "Invalid option"
+                ;;
+        esac
+    done
+}
+
 # Function to install Zabbix Agent 2
 install_zabbix_agent() {
+    local version="$1"
+    local package_url
+
+    case "$version" in
+        "6.0")
+            package_url="https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-5+debian12_all.deb"
+            ;;
+        "6.4")
+            package_url="https://repo.zabbix.com/zabbix/6.4/debian/pool/main/z/zabbix-release/zabbix-release_6.4-1+debian12_all.deb"
+            ;;
+        *)
+            echo "Unsupported version: $version"
+            return 1
+            ;;
+    esac
+
     # Add Zabbix repository
-    wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-5+debian12_all.deb
-    dpkg -i zabbix-release_6.0-5+debian12_all.deb
+    wget "$package_url"
+    dpkg -i "$(basename "$package_url")"
     apt update
 
     # Install Zabbix Agent 2
@@ -32,22 +73,23 @@ install_zabbix_agent() {
 configure_zabbix_agent() {
     # Prompt for Zabbix Server IP
     echo
-    read -p "Enter Zabbix Server IP [$default_zabbix_server_ip]: " zabbix_server_ip
-    zabbix_server_ip="${zabbix_server_ip:-$default_zabbix_server_ip}"
+    read -p "Enter Zabbix Server IP [$default_zabbix_server_address]: " zabbix_server_address
+    zabbix_server_address="${zabbix_server_address:-$default_zabbix_server_address}"
 
     # Set hostname
     read -p "Enter hostname for this agent [$default_agent_hostname]: " agent_hostname
     agent_hostname="${agent_hostname:-$default_agent_hostname}"
     # Configure Zabbix Agent 2
-    sed -i "s/^Server=127.0.0.1/Server=$zabbix_server_ip/" /etc/zabbix/zabbix_agent2.conf
-    sed -i "s/^ServerActive=127.0.0.1/ServerActive=$zabbix_server_ip/" /etc/zabbix/zabbix_agent2.conf
+    sed -i "s/^Server=127.0.0.1/Server=$zabbix_server_address/" /etc/zabbix/zabbix_agent2.conf
+    sed -i "s/^ServerActive=127.0.0.1/ServerActive=$zabbix_server_address/" /etc/zabbix/zabbix_agent2.conf
     sed -i "s/^Hostname=.*/Hostname=$agent_hostname/" /etc/zabbix/zabbix_agent2.conf
 }
 
-# Function to start Zabbix Agent 2
-start_zabbix_agent() {
-    systemctl start zabbix-agent2
-    systemctl enable zabbix-agent2
+# Function to stop Zabbix Agent 2
+stop_zabbix_agent() {
+#    systemctl start zabbix-agent2
+#    systemctl enable zabbix-agent2
+    systemctl stop zabbix-agent2
     systemctl status zabbix-agent2
 }
 
@@ -60,13 +102,13 @@ main() {
     install_zabbix="${install_zabbix:-Y}" # Default to yes if no option provided
 
     if [[ $install_zabbix =~ ^[Yy]$ ]]; then
-        install_zabbix_agent
+        display_menu
     else
         echo "Zabbix Agent installation skipped."
     fi
 
     configure_zabbix_agent
-    start_zabbix_agent
+    stop_zabbix_agent
 
     echo "Zabbix Agent 2 installed and configured successfully."
     echo
@@ -76,11 +118,14 @@ main() {
     echo "### May be you need these commands ###"
     echo
     echo "# vim /etc/zabbix/zabbix_agent2.conf"
+    echo
     echo "# tail -f /var/log/zabbix/zabbix_agent2.log"
     echo "# zabbix_agent2 -p | head"
+    echo
     echo "# systemctl stop zabbix-agent2"
     echo "# systemctl status zabbix-agent2"
     echo "# systemctl start zabbix-agent2"
+    echo "# systemctl enable zabbix-agent2"
     echo
 }
 
